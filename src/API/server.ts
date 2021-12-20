e09c1933618b5cd57fec2295276e3837b4586ea1
Dell <dell@DESKTOP-NLH0UUR>
2021-12-20 15:42:55 +0100

    add API server + initial app structure

---
+++ src/API/server.ts
@@ -0,0 +1,18 @@
+import { createServer, Model } from "miragejs";
+import {v4 as uuid} from "uuid";
+
+createServer({
+    models :{
+        todos : Model,
+    },
+    seeds(server){
+        server.create("todo", { id: uuid(), text: "Buy wheat", done: false });
+        server.create("todo", { id: uuid(), text: "Make bread", done: true });
+    },
+    routes(){
+        this.get("todos/", (schema) => {
+            return schema.db.todos;
+          });
+    }
+}
+)
---
+++ src/Models/todoitem.ts
@@ -0,0 +1,5 @@
+export default interface ToDoItem {
+    id : string,
+    text: string,
+    done: boolean
+}
